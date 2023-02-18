920d0fd89b84936c9a9cd061e2197c9601209835
shdb <shadowblade@gmx.ch>
2023-02-18 23:27:18 +0100

    use callbacks to track mpris changes

--- shiny/player.lua
+++ shiny/player.lua
@@ -29,26 +29,50 @@ function player:init_player(name)
     local playerctl = self.Playerctl.Player.new_from_name(name)
     self.manager:manage_player(playerctl)
     playerctl.on_metadata = function(playerctl, metadata)
-        self:update(playerctl)
+        self:metadata_cb(playerctl, metadata)
     end
 
-    playerctl.on_playback_status = function(playerctl, playback_status)
-        self:update(playerctl)
-        self.active = playerctl
+    playerctl.on_playback_status = function(playerctl, status)
+        self:status_cb(playerctl, status)
     end
 
     playerctl.on_seeked = function(playerctl, position)
-        self:update(playerctl)
-        self.active = playerctl
+        self:update(playerctl, true)
     end
 
     playerctl.on_exit = function(playerctl)
         self.textbox:set_markup('')
         self.active = nil
+        gears.timer.delayed_call(function()
+            self:init_data()
+        end)
     end
+
+    self.active = playerctl
+    self:init_data(playerctl)
 end
 
-function player:update(playerctl)
+function player:metadata_cb(playerctl, metadata)
+    local data = metadata.value
+    self.title = data["xesam:title"] or ""
+    self.artist = data["xesam:artist"][1] or ""
+    self.length = (data["mpris:length"] or 0) / 10^6
+    self:update(playerctl)
+end
+
+function player:status_cb(playerctl, status)
+    self.status = status
+    self:update(playerctl, true)
+end
+
+function player:playerctl_exists(playerctl)
+    for _, lplayerctl in ipairs(self.manager.players) do
+        if lplayerctl == playerctl then return true end
+    end
+    return false
+end
+
+function player:init_data(playerctl)
     if not playerctl then
         if not (self.active or self.manager.players[1]) then
             self.textbox:set_markup()
@@ -58,28 +82,56 @@ function player:update(playerctl)
         end
     end
 
-    local status = playerctl.playback_status
-    local artist = playerctl:get_artist() or ''
-    local title = playerctl:get_title() or ''
+    if not player:playerctl_exists(playerctl) then return end
+
+    self.status = playerctl.playback_status
+    self.artist = playerctl:get_artist() or ''
+    self.title = playerctl:get_title() or ''
+    self.length = (playerctl.metadata.value["mpris:length"] or 0) / 10^6
+
+    self:update(playerctl)
+end
+
+function player:update(playerctl, checkactive)
+    if checkactive and playerctl and self.active ~= playerctl then
+        self.active = playerctl
+        self:init_data(playerctl)
+    end
+    if not playerctl then
+        if not (self.active or self.manager.players[1]) then
+            self.textbox:set_markup()
+            return
+        else
+            playerctl = self.active or self.manager.players[1]
+        end
+    end
+
+    if not player:playerctl_exists(playerctl) then return end
+
+    local artist = gears.string.xml_escape(self.artist)
+    local title = gears.string.xml_escape(self.title)
     local position = (playerctl:get_position() or 0) / 10^6
-    local length = (playerctl.metadata.value["mpris:length"] or 0) / 10^6
+    if self.length == 0 then
+        self.length = (playerctl.metadata.value["mpris:length"] or 0) / 10^6
+    end
+    local length = self.length
 
     local separatorartist = (artist == '' or title == '') and '' or shiny.fg(beautiful.highlight, ' / ')
     local separatortime = (length == 0 or position == 0) and '' or shiny.fg(beautiful.highlight, ' / ')
     local separatorposition = (length == 0 or position == 0) and '' or shiny.fg(beautiful.highlight, ' | ')
     local space = (length == 0 and position == 0 and artist == '' and title == '') and '' or ' '
 
-    if status == nil or status == '' then
+    if self.status == nil or self.status == '' then
         self.statusicon:set_image(beautiful.player_stop)
-    elseif status == 'PAUSED' then
+    elseif self.status == 'PAUSED' then
         self.statusicon:set_image(beautiful.player_pause)
-    elseif status == 'PLAYING' then
+    elseif self.status == 'PLAYING' then
         self.statusicon:set_image(beautiful.player_play)
     else
         self.statusicon:set_image(beautiful.player_stop)
     end
 
-    self.textbox:set_markup(space .. gears.string.xml_escape(artist) .. separatorartist .. gears.string.xml_escape(title) .. separatorposition
+    self.textbox:set_markup(space .. artist .. separatorartist .. title .. separatorposition
         .. self:formattime(position) .. separatortime .. self:formattime(length))
 end
 
@@ -140,6 +192,10 @@ function player:new()
     self.Playerctl = lgi.Playerctl
     self.manager = self.Playerctl.PlayerManager()
     self.active = nil
+    self.artist = ''
+    self.title = ''
+    self.length = 0
+    self.position = 0
 
     local _self = self
     -- manage existing players on startup
